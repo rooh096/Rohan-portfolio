d28f51e25d24311867d30e6764e2473102ef6966
rooh096 <rohan.agarwal0596@gmail.com>
2026-03-24 01:26:58 +0530

    Create Home component for portfolio page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Rohan Agarwal Portfolio</h1>
+      <p>Website is loading successfully 🚀</p>
+    </main>
+  );
+}
